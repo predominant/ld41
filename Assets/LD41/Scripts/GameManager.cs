f465ffe145db1a135b0178a0d61f9fa5c4421f35
Graham Weldon <graham@grahamweldon.com>
2018-04-22 23:04:41 +0900

    Added post-processing, and the beginning of match timer
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- Assets/LD41/Scripts/GameManager.cs
+++ Assets/LD41/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,14 +12,20 @@ namespace Assets.LD41.Scripts
     {
         [SerializeField] private GameObject EndGamePanel;
         [SerializeField] private GameObject WinGamePanel;
+        [SerializeField] private TextMeshProUGUI TimerText;
 
         private static GameManager _instance;
+        private bool _gameEnded;
+        private float _startTime;
+        private float _totalTime;
 
         private void Awake()
         {
             if (_instance == null)
             {
                 _instance = this;
+                this._startTime = Time.time;
+                this.StartCoroutine("Timer");
                 return;
             }
 
@@ -50,6 +58,32 @@ namespace Assets.LD41.Scripts
         public void ExitGame()
         {
             SceneManager.LoadScene("Menu");
+        }
+
+        private IEnumerator Timer()
+        {
+            while (!this._gameEnded)
+            {
+                yield return new WaitForEndOfFrame();
+
+                var timer = Time.time - this._startTime;
+
+                var m = Mathf.Floor(timer / 60);
+                var s = Mathf.RoundToInt(timer % 60);
+
+                string minutes, seconds;
+
+                minutes = m.ToString();
+                if (m < 10)
+                    minutes = "0" + m;
+
+                seconds = s.ToString();
+                if (s < 10)
+                    seconds = "0" + Mathf.RoundToInt(s);
+
+                this.TimerText.text = String.Format("{0}:{1}", minutes, seconds);
+            }
+            this._totalTime = Time.time - this._startTime;
         }
     }
 }
